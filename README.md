ec0daf5ae01bf3f64c7961df456ba8b94f0a0583
Drew Romanyk <drewiswaycool@gmail.com>
2021-07-24 01:16:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# What we doing tonight
+
+This repo is the source code for the webpage to help decide thursday game nights
---
+++ index-2021-07-24.js
@@ -0,0 +1,43 @@
+function shuffle(array) {
+    var currentIndex = array.length, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+var index = 4;
+var randomSeed = "20210730";
+Math.seedrandom(randomSeed);
+
+var choosenerOptions = shuffle([
+    "Drew",
+    "Jeff",
+    "Matt",
+    "Mike"
+]);
+
+var foodOptions = shuffle([
+    "Ezells",
+    "Arbys",
+    "Five Guys",
+    "Mod Pizza",
+    "Wibbleys"
+]);
+
+window.onload = init;
+
+function init() {
+    if (index == choosenerOptions.length) {
+        document.getElementById("choosener").innerHTML = "Y'ALL GOIN TO MOX!";
+    } else {
+        document.getElementById("choosener").innerHTML = "Choosener: " + choosenerOptions[index];
+        document.getElementById("food").innerHTML = "Food: " + foodOptions[index];
+    }
+
+}
